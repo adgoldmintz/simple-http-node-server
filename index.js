9e823cbf130b5ad0991c795f6ba9f9012ee25b6b
AGoldmintz <agoldmintz@gmail.com>
2018-12-07 19:01:33 -0600

    Creates .txt file
    Updates to start value in package.json and tested for efficacy

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const http = require('http');
 const port = 3000;
 
+const fs = require('fs');
+
 const requestHandler = (request, response) => {
     fs.writeFile('hello-world.txt', 'Hello to this great world', 'utf8', (err) => {
         if (err) throw err;
